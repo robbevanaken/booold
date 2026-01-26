e42faef4815be78c05c99c11c2d6050a34f9909e
Robbe <robbe@weareantenna.be>
2026-01-26 18:55:42 +0100

    chore: tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         "base": ["clamp(14px, 1.5vw, 16px)", 1.5],
         "md": ["clamp(16px, 1.55vw, 22px)", 1.5],
         "lg": ["clamp(24px, 2.5vw, 72px)", 1.7],
-        "lg-scaleup": ["clamp(28px, 3vw, 100px)", 1.7],
+        "lg-scaleup": ["clamp(28px, 2.75vw, 78px)", 1.7],
         "xl": ["clamp(28px, 3vw, 56px)", 1.25],
         "xxl": ["clamp(40px, 5vw, 64px)", 1.15],
         "xxxl": ["clamp(64px, 6vw, 94px)", 1.15],
