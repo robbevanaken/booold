2f551e61eddcbb11dab33545dcbd3cc7417715b7
Robbe <robbe@weareantenna.be>
2026-01-26 18:04:09 +0100

    chore: showreel

---
+++ app/components/HomeQuote.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="c-quote"></div>
+</template>
+
+<script>
+export default {
+  name: 'HomeQuote',
+}
+</script>
--- app/components/HomeReel.vue
+++ app/components/HomeReel.vue
@@ -1,13 +1,46 @@
 <template>
   <div data-theme-section="dark" class="o-container o-grid">
     <div class="col-span-12 lg:col-span-10 lg:col-start-2">
-      <div class="c-reel"></div>
+      <div class="c-reel">
+        <Swiper
+          :modules="[Autoplay]"
+          :slides-per-view="1"
+          :loop="true"
+          :autoplay="{
+            delay: 1000,
+            disableOnInteraction: false
+          }"
+          :speed="1"
+          class="c-reel__swiper"
+        >
+          <SwiperSlide v-for="(image, index) in images" :key="index">
+            <img :src="image.src" :alt="image.alt" class="c-reel__image" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HomeReel',
-}
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Autoplay } from 'swiper/modules'
+import 'swiper/css'
+
+const images = [
+  { src: '/images/reel/mockup-dwb.webp', alt: 'Website for De Wilde - Braems' },
+  { src: '/images/reel/mockup-ngis.webp', alt: 'Website for NGIS Wireless' },
+]
 </script>
+
+<style>
+.c-reel__swiper {
+  width: 100%;
+}
+
+.c-reel__image {
+  width: 100%;
+  height: auto;
+  display: block;
+}
+</style>
