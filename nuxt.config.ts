ea208c58aeace632c9c0bbe99842922b6f91565d
Robbe <robbe@weareantenna.be>
2026-02-02 16:38:45 +0100

    chore: temp disable GA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,5 +21,22 @@ export default defineNuxtConfig({
 
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
+//     head: {
+//       script: [
+//         {
+//           innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+// new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+// j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+// 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+// })(window,document,'script','dataLayer','GTM-TXRPTPKT');`,
+//         }
+//       ],
+//       noscript: [
+//         {
+//           innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TXRPTPKT" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+//           tagPosition: 'bodyOpen'
+//         }
+//       ]
+//     }
   },
 })
